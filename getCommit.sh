a7438c69fe11ce98b4ca222253f79fd4f40bbcf7
Akash-Lakshman <lakshmaa@tcd.ie>
2017-12-14 15:38:32 +0000

    Shell file for getting Commits to the RepoData folder and Update on WorkerCode

---
+++ getCommit.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd repoData
+git reset --hard $1
--- workerNode.py
+++ workerNode.py
@@ -21,6 +21,19 @@ def exec():
     print(" end of segment!!! ")
     req = requests.get(link,json = {'pullState' : True})
     
+    while True:
+        cyclo = requests.get(cyclomaticURL)
+        print("Got Cyclomatic",cyclo)
+        json_data = json.loads(cyclo.text)
+        print(json_data)
+        print("Received: ",json_data['sha'])
+        if json_data['sha'] == -2:       # Polling for manager to give commits
+            print("Polling")
+        else:
+            if json_data['sha'] == -1:
+                print("END, Nothing Remaining")
+                break
+            subprocess.call(["bash", "getCommit.sh", json_data['sha']])
     
 if __name__ == "__main__":
     exec()
